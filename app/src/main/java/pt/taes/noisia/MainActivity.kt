ce0c0238890704ed149b67aeee45e018a3f2e1b2
Andre Azevedo <46000644+ALGAzevedo@users.noreply.github.com>
2020-10-31 17:32:58 +0000

    Initial commit

---
+++ app/src/main/java/pt/taes/noisia/MainActivity.kt
@@ -0,0 +1,91 @@
+package pt.taes.noisia
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.lang.Exception
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    private var mediaRecorder: MediaRecorder? = null
+    private var state: Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonStart).setOnClickListener {
+
+            Thread{
+                startRecording()
+            }.start()
+
+            }
+
+        findViewById<Button>(R.id.buttonStop).setOnClickListener {
+            stopRecording()
+        }
+
+
+
+    }
+
+
+    private fun startRecording() {
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        } else {
+
+            mediaRecorder = MediaRecorder()
+            mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            mediaRecorder?.setOutputFile("/dev/null");
+
+            try {
+                mediaRecorder?.prepare()
+            } catch (ioe: IOException) {
+                Log.e("[Monkey]", "IOException: " + Log.getStackTraceString(ioe))
+            } catch (e: SecurityException) {
+                Log.e("[Monkey]", "SecurityException: " + Log.getStackTraceString(e))
+            }
+            try {
+                mediaRecorder?.start()
+                state = true
+
+                Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
+            } catch (e: SecurityException) {
+                Log.e("[Monkey]", "SecurityException: " + Log.getStackTraceString(e))
+            }catch (e: Exception){
+                Log.e("[Monkey]", "Exception: " + Log.getStackTraceString(e))
+            }
+        }
+    }
+
+
+    private fun stopRecording(){
+        if(state){
+            mediaRecorder?.stop()
+            mediaRecorder?.release()
+            state = false
+        }else{
+            Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
